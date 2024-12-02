aad63507344766fd7143b49ed829f3d28dc43c36
Iddrisusalima <iddrisusalima733@gmail.com>
2024-12-02 12:46:15 +0000

    adding all files

---
+++ src/style.js
@@ -0,0 +1,29 @@
+let myname ="Salima Iddrissu";
+console.log(myname);
+
+let myAge = 19;
+console.log(myAge);
+
+let sum = 15 + 7;
+console.log(sum);
+
+let difference = 20 -8;
+console.log(difference);
+
+let quotient = 50 / 10;
+console.log(quotient);
+
+let product = 7 * 6;
+console.log(product);
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+let isLearningJS = true;
+console.log(isLearningJ);
+
+let numberVariable = 100;
+console.log(numbervariable);
+
+let stringVariable = "I am a beautiful girl";
+console.log(stringvariable);
